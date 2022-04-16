5e82f09213304a11de21e0953aab276991dcf32d
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-17 01:48:16 +0600

    navbar fixed

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,29 +5,47 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand to="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <NavLink
-              to="/home"
-              className="text-decoration-none mx-3 text-light"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/about"
-              className="text-decoration-none mx-3 text-light"
-            >
-              About
-            </NavLink>
-            <NavLink
-              to="/services"
-              className="text-decoration-none mx-3 text-light"
-            >
-              Services
-            </NavLink>
-          </Nav>
+          <Navbar.Brand to="/home">Power Zone</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink
+                to="/home"
+                className="text-decoration-none mx-3 text-light"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/services"
+                className="text-decoration-none mx-3 text-light"
+              >
+                Services
+              </NavLink>
+              <NavLink
+                to="/checkout"
+                className="text-decoration-none mx-3 text-light"
+              >
+                Checkout
+              </NavLink>
+            </Nav>
+            <Nav>
+              <NavLink
+                to="/login"
+                className="text-decoration-none mx-3 text-light"
+              >
+                Login{" "}
+              </NavLink>
+              <NavLink
+                eventKey={2}
+                to="/signup"
+                className="text-decoration-none mx-3 text-light"
+              >
+                Sign up
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <br />
