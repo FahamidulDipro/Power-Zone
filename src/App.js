e72c4ab9841d10fb7fbebd451bd37143c490ceed
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-17 01:31:25 +0600

    Created context api to pass data to different components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { createContext, useEffect, useState } from 'react';
+
+export const TrainerLoad = createContext();
 
 function App() {
+  const [trainers,setTrainers] = useState([]);
+  useEffect(()=>{
+    fetch('data.json').then(res=>res.json()).then(data=>setTrainers(data));
+  },[])
+ 
   return (
+    <TrainerLoad.Provider value={trainers}>
     <div className="App">
       
     </div>
+    </TrainerLoad.Provider>
   );
 }
 
