e82e0535171bfcdb3d2aaacab2b341360c5dd056
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-17 12:27:39 +0600

    trainer name and detail shown in checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { TrainerLoad } from "../../App";
 
 const Checkout = () => {
-    const {trainerId} = useParams();
-    return (
-        <div className='mt-5'>
-            <h1>Checkout</h1>
-            {
-                console.log(trainerId)
-            }
-        </div>
-    );
+  const trainers = useContext(TrainerLoad);
+  const { trainerId } = useParams();
+  const trainerDetail = trainers.find((trainer) => trainer.id === trainerId);
+  
+  return (
+    <div style={{ marginTop: "200px" }}>
+      {trainerDetail ? (
+        <section className="detail-section text-start border container mt-5 d-flex justify-content-between p-3 flex-lg-row flex-md-row flex-sm-column flex-column">
+          <div className='w-50'>
+            <h1>{trainerDetail?.name}</h1>
+          </div>
+          <div className='w-50'>
+            <img src={trainerDetail?.picture} alt="trainerPicture" className='img-fluid'/>
+          </div>
+        </section>
+      ) : (
+        <h1>No Trainer Selected</h1>
+      )}
+    </div>
+  );
 };
 
 export default Checkout;
