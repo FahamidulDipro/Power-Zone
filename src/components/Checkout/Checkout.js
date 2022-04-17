de18f9b32ea040da23b1b925fd7ba35a9442dd8f
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-17 12:06:40 +0600

    clicking on Learn More button redirects to the checkout page with unique trainer id

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Checkout = () => {
+    const {trainerId} = useParams();
     return (
         <div className='mt-5'>
             <h1>Checkout</h1>
+            {
+                console.log(trainerId)
+            }
         </div>
     );
 };
