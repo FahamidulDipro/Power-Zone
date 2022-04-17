d463767f1a8a89ed2de787778ca052cc9b61c610
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-17 13:07:54 +0600

    signup page designed

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,59 @@
-import React from 'react';
-
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./Signup.css";
 const Signup = () => {
-    return (
-        <div className='mt-5'>
-            <h1>Signup</h1>
-        </div>
-    );
+  return (
+    <div
+      className=" container d-flex justify-content-center"
+      style={{ marginTop: "200px" }}
+    >
+      <section className="w-50">
+        <h1 className="text-start mb-5 text-light">Please Signup</h1>
+        <Form className="text-start  ">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="text"
+              placeholder="Name"
+              className="border-0 shadow-lg p-3"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Email"
+              className="border-0 shadow-lg p-3"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className="border-0 shadow-lg p-3"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              className="border-0 shadow-lg p-3"
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicCheckbox"
+          ></Form.Group>
+          <Button
+            variant="success"
+            type="submit"
+            className="w-100 p-3 fw-bold "
+          >
+            Submit
+          </Button>
+        </Form>
+      </section>
+    </div>
+  );
 };
 
 export default Signup;
