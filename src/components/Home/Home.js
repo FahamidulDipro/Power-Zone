0ed60e1de0ada350947e718efd7c31cd46f5b831
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-18 15:20:00 +0600

    Slider added in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ServicesLoad from '../ServicesLoad/ServicesLoad';
+import Slider from '../Slider/Slider';
 
 const Home = () => {
     return (
         <div className='mt-5'>
+            <Slider></Slider>
            <ServicesLoad></ServicesLoad>
         </div>
     );
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+const Slider = () => {
+    return (
+        <Carousel >
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1604233098531-90b71b1b17a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="First slide" style={{height:'90vh'}}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1637666218229-1fe0a9419267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="Second slide" style={{height:'90vh'}}
+          />
+      
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.unsplash.com/photo-1630703178161-1e2f9beddbf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="Third slide" style={{height:'90vh'}}
+          />
+      
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+};
+
+export default Slider;
