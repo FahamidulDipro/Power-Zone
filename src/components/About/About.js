b0f6664d9bb95765d55557715bddd37e69a4e6c7
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-18 20:04:26 +0600

    about section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='mt-5'>
-            <h1>About</h1>
+        <div style={{marginTop:'100px'}} className='container text-light'>
+            <h1 className=' text-start'>About Me</h1>
+            <hr />
+            <section className='text-start d-flex justify-content-between flex-lg-row flex-md-row flex-sm-column flex-column p-3'>
+                <div className='order-1 m-3'>
+                    <p>Hi! I am <b>Md. Fahamidul Islam</b>. I am a person with a passion for developing scalable web applications and also working accross the full stack. I have designed 20+ front end UIs and currently focusing on database and backend. I have extensive experience working with Javascript and React.js. Currently I am trying my best to build some unique web applications with React.js and I would jump at any opportunity to become an experienced fullstack web app developer to continue growing my skillset in the JS field. </p>
+                </div>
+                <div className='m-3'> 
+                    <img src='../images/myPhoto.jpg' alt="MyPhoto" className='img-fluid rounded'/>
+                </div>
+            </section>
         </div>
     );
 };
